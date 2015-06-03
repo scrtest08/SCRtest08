0a638153af20424b1162bc5d2b0b8ce8f8a1843e
Taagart <taagart@gmail.com>
2015-06-03 17:58:27 +0200

    Dokoncz zdanie: Gdyby babcia miala wasy to bylaby ...

--- src/calc.cpp
+++ src/calc.cpp
@@ -3,13 +3,22 @@
 int Calculator::pow(int a, int n)
 {
 	// ZAIMPLEMENTUJ MNIE ;_;
-
+  std::cout << "sdasvfgds";
 	return -1;
 }
 	
 int Calculator::fac(int n)
 {
 	// ZAIMPLEMENTUJ MNIE ;_;
-
+//dfasbhjdfasgdsjvgdfsjhgvdfjsgjvdfs
 	return -1;
 }
+
+
+
+
+
+
+
+
+
